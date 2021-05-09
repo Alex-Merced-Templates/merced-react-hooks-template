33ab51e6903193d0fd7eb9468f0944da573340a0
Alex Merced <alex@alexmerced.dev>
2021-05-09 12:18:53 -0400

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
-import {useObject} from "merced-react-hooks"
+import { useMap, useArray, Loop } from "merced-react-hooks";
 
-function Home(props){
+function Home(props) {
+  const [state, setKey] = useMap({ count: 1 });
+  const [ arr, setArr] = useArray([1,2,3,4,5])
 
-    const [state, setKey] = useObject({count: 1})
 
-    return <div>
-        <h1>{state.count}</h1>
-        <button onClick={() => setKey("count", state.count + 1)}>Add</button>
-        <button onClick={() => setKey("count", state.count - 1)}>Add</button>
+  return (
+    <div>
+      <h1>{state.get("count")}</h1>
+      <button onClick={() => setKey("count", state.get("count") + 1)}>
+        Add
+      </button>
+      <button onClick={() => setKey("count", state.get("count") - 1)}>
+        Add
+      </button>
+      <Loop withthis={arr} dothat={(item) => <h2>{item}</h2>}/>
+      <button onClick={() => setArr.push(arr.length+1)}>
+        Add
+      </button>
     </div>
+  );
 }
 
-export default Home
+export default Home;
